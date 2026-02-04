98e227bd4e441909de6f366749283f7c58674550
Luka <luka.lopez.j@gmail.com>
2026-02-04 11:25:21 +0100

    Pantalla de login acabada, botón de continuar con número de teléfono implementado, el resto están en TODO

--- app/src/main/java/com/mireyaserrano/linder/MainActivity.kt
+++ app/src/main/java/com/mireyaserrano/linder/MainActivity.kt
@@ -4,13 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.mireyaserrano.linder.databinding.ActivityMainBinding
 import com.mireyaserrano.linder.ui.auth.LoginActivity
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/mireyaserrano/linder/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/mireyaserrano/linder/ui/auth/LoginActivity.kt
@@ -1,4 +1,45 @@
 package com.mireyaserrano.linder.ui.auth
 
-class LoginActivity {
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val btnGoogle = findViewById<MaterialButton>(R.id.btn_google)
+        val btnFacebook = findViewById<MaterialButton>(R.id.btn_facebook)
+        val btnPhone = findViewById<MaterialButton>(R.id.btn_phone)
+        val btnTroubles = findViewById<MaterialButton>(R.id.btn_troubles)
+
+        val loginWithFacebook = {
+            //TODO: Hacer el registro con Facebook
+            Toast.makeText(this, "Opción no disponible todavía", Toast.LENGTH_SHORT).show()
+        }
+
+        val loginWithGoogle = {
+            //TODO: Hacer el registro con Google
+            Toast.makeText(this, "Opción no disponible todavía", Toast.LENGTH_SHORT).show()
+        }
+
+        val contactWithSupport = {
+            //TODO: Solicitar ayuda al soporte
+            Toast.makeText(this, "Opción no disponible todavía", Toast.LENGTH_SHORT).show()
+        }
+
+        btnGoogle.setOnClickListener { loginWithGoogle() }
+        btnFacebook.setOnClickListener { loginWithFacebook() }
+        btnTroubles.setOnClickListener { contactWithSupport() }
+
+        btnPhone.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
